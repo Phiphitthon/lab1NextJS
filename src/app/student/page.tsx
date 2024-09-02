6fefbd8fdc1bd57660afed6e49905928807f3de6
HoopShop.TH <119285445+Phiphitthon@users.noreply.github.com>
2024-09-02 20:01:55 +0700

    first commit

---
+++ src/app/student/page.tsx
@@ -0,0 +1,11 @@
+export default function AboutMePage() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>Name: Pipithatna Pipithatkul</p>
+      <p>Student ID: 653450097-2</p>
+      <p>HoopShop.th</p>
+      <p>CS</p>
+    </div>
+  );
+}
